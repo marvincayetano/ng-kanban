1d965001a289286d78a65b3427f2b6c5a7d14c51
Marvin Cayetano <cayetanomarvin@gmail.com>
2022-02-18 19:15:05 -0500

    📄 Note: CDK Drag & drop exists

--- src/app/pages/main-view/main-view.component.ts
+++ src/app/pages/main-view/main-view.component.ts
@@ -15,7 +15,6 @@ export class MainViewComponent implements OnInit {
 
   ngOnInit(): void {}
   todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
-
   done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
 
   drop(event: CdkDragDrop<string[]>) {
